38fdcfb5d3ade588ea42cf7ea81513c9d5564b2f
Thom Benjamins <thom.benjamins@nationwide.co.uk>
2022-11-23 16:54:32 +0000

    Added env_vars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hello-world-challenge
+
+# For accessing .env file
+pip install python-dotenv
--- app.py
+++ app.py
@@ -1,25 +1,35 @@
+#Import logging for standard logging
 import logging
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], help="Set the logging level")
+#Import os and line 4 to access .env file for environment variables
+import os
+from dotenv import load_dotenv
 
-args = parser.parse_args()
-if args.logLevel:
-    logging.basicConfig(level=getattr(logging, args.logLevel))
-    
+#import argparse
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], help="Set the logging level")
+
+#args = parser.parse_args()
+#if args.logLevel:
+#    logging.basicConfig(level=getattr(logging, args.logLevel))
 
 #Create and config logger
 logging.basicConfig(filename='std.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
 
 logger=logging.getLogger() 
 
 #Set threshold of logger to DEBUG 
 logger.setLevel(logging.DEBUG) 
 
+load_dotenv() 
+ 
+#Print env_var from .env
+print(os.environ['NAME'])  
 #To local file
-logger.info("Hello World! and more")
+logger.info("Hello World!")
+logger.info(os.environ['NAME'])
+logger.info(os.environ['MESSAGE'])
 
 #To the console
 print("Hello world!")
