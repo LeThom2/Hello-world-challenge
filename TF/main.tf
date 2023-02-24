c8f5e2a69e3f8ffe2cfcbe8d37ce9a63000fd05e
Thom Benjamins <thom.benjamins@nationwide.co.uk>
2023-02-24 21:43:29 +0000

    Working servers output to elastic

--- TF/main.tf
+++ TF/main.tf
@@ -2,57 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "EC2-TF" {
+resource "aws_instance" "logstash_server" {
   ami             = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.TF_security_group.name]
   key_name = "TF_key"
-  user_data = file("filebeat.sh")
+  user_data = file("logstash.sh")
+  private_ip = "172.31.13.1"
 
   tags = {
-    Name = "EC2-TF"
+    Name = "logstash_server"
   }
 }
-
-
-resource "aws_security_group" "TF_security_group" {
-  name        = "security group using terraform"
-  description = "security group using terraform"
-  vpc_id      = "vpc-02d198584f7bcf660"
-
-  #inbound rules
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  #outbound rules
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_instance" "EKF_server" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.TF_security_group.name]
+  key_name = "TF_key"
+  user_data = file("EKF.sh")
+  private_ip = "172.31.13.2"
 
   tags = {
-    Name = "TF_security_group"
+    Name = "EKF_server"
   }
 }
 
@@ -71,6 +42,7 @@ resource "local_file" "TF_key" {
   filename = "tfkey"
 }
 
+# Copy a file with Terraform as part of Hello World Challange part 1
 # resource "local_file" "HWC_Copy" {
 #         content     = file ("/Users/Thom.Benjamins/Desktop/HWC/app.py")
 #         filename    = "/Users/Thom.Benjamins/Desktop/HWC-Copy/app_copy.py"
