2735463292a9e30362168ecd1b37cd0ceadbde8d
Thom Benjamins <thom.benjamins@nationwide.co.uk>
2023-03-20 16:41:51 +0000

    Changed path in .yml file for playbook to work

--- TF/main.tf
+++ TF/main.tf
@@ -1,6 +1,23 @@
+# terraform {
+#   required_providers {
+#     elasticsearch = {
+#       source = "disaster37/elasticsearch"
+#       version = "8.4.0"
+#       }
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.53.0"
+#       }
+#   }
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
+
+# provider "elasticsearch" {
+#     urls     = "http://44.213.113.90:9200"
+# }
 
 resource "aws_instance" "logstash_server" {
   ami             = var.ami
@@ -43,31 +60,21 @@ resource "local_file" "TF_key" {
   filename = "tfkey"
 }
 
-# terraform {
-#   required_providers {
-#     elasticsearch = {
-#       source = "disaster37/elasticsearch"
-#       version = "8.4.0"
-#       urls     = "${PUBLIC_IP}"
 
-#       }
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.53.0"
-#       }
-#   }
-# }
-# resource elasticsearch_index_lifecycle_policy "ilm" {
+# resource elasticsearch_index_lifecycle_policy "my_ilm" {
 #   name = "terraform-ilm"
 #   policy = <<EOF
 # {
 #   "policy": {
+#     "phases": {
+#       "hot": {},
 #       "delete": {
 #         "min_age": "1d",
 #         "actions": {
 #           "delete": {}
 #         }
 #       }
+#     }
 #   }
 # }
 # EOF
