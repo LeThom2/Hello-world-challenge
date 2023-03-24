5fbc1089b2c6c0eae242bf3dbab9ae8aaedeab7b
thom <thom.benjamins@nationwide.co.uk>
2023-03-24 08:42:15 +0000

    HWC1 tests

--- HWC1/main.tf
+++ HWC1/main.tf
@@ -1,6 +1,6 @@
 # Copy a file with Terraform as part of Hello World Challange part 1
 
 resource "local_file" "HWC_Copy" {
-  content  = file("/Users/Thom.Benjamins/Desktop/HWC/app.py")
+  content  = file("/Users/Thom.Benjamins/Desktop/HWC/HWC1/app.py")
   filename = "/Users/Thom.Benjamins/Desktop/HWC-Copy/app_copy.py"
 }
