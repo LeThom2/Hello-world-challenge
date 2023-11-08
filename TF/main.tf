c085cebebfab664544cc1b0d4c44c78dbd222463
Thom-Benjamins_nbs <thom.benjamins@nationwide.co.uk>
2023-11-08 16:33:11 +0000

    Rid old config

--- TF/main.tf
+++ TF/main.tf
@@ -1,23 +1,6 @@
-# terraform {
-#   required_providers {
-#     elasticsearch = {
-#       source = "disaster37/elasticsearch"
-#       version = "8.4.0"
-#       }
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.53.0"
-#       }
-#   }
-# }
-
 provider "aws" {
   region = "us-east-1"
 }
-
-# provider "elasticsearch" {
-#     urls     = "http://44.213.113.90:9200"
-# }
 
 resource "aws_instance" "logstash_server" {
   ami             = var.ami
@@ -59,23 +42,3 @@ resource "local_file" "TF_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tfkey"
 }
-
-
-# resource elasticsearch_index_lifecycle_policy "my_ilm" {
-#   name = "terraform-ilm"
-#   policy = <<EOF
-# {
-#   "policy": {
-#     "phases": {
-#       "hot": {},
-#       "delete": {
-#         "min_age": "1d",
-#         "actions": {
-#           "delete": {}
-#         }
-#       }
-#     }
-#   }
-# }
-# EOF
-# }
